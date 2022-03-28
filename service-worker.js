a56423c3b39e02a710db91275ac5295ab9d1c718
Rich <richd@L31604MUS.local>
2022-03-28 13:42:40 -0400

    first commit

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+importScripts("./precache-manifest.bccbb9358aac92ea3682a9b2a5b39744.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* global workbox self */
+/* eslint no-restricted-globals: 0  */
+/* eslint no-underscore-dangle: 0  */
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+workbox.core.setCacheNameDetails({ prefix: 'upluswss' });
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+self.addEventListener('message', (msg) => {
+  if (msg.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
